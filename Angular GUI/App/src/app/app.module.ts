92c382574d2e9360a1afcf9a313b59f805a0a48a
Nazhim <nazhimkalam@gmail.com>
2020-11-24 20:35:08 +0530

    created the welcome page

--- Angular GUI/App/src/app/app.module.ts
+++ Angular GUI/App/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
