3abaf1d8878136e2f003467b1f428c41947fc267
Nazhim <nazhimkalam@gmail.com>
2020-11-25 20:56:33 +0530

    completed the nav bar

--- Angular GUI/App/src/app/app.module.ts
+++ Angular GUI/App/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AboutComponent } from './about/about.component';
+import { TableComponent } from './table/table.component';
+import { MatchesComponent } from './matches/matches.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     NavBarComponent,
-    AboutComponent
+    AboutComponent,
+    TableComponent,
+    MatchesComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
--- Angular GUI/App/src/app/nav-bar/nav-bar.component.ts
+++ Angular GUI/App/src/app/nav-bar/nav-bar.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  linkNames: string[] = ['about', 'table', 'matches', 'players'];
+  activeLinks: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.activeLinks[0] = 'active'
   }
 
+  // THIS IS TO MAKE THE ACTIVE LINKS VISIBLE IN THE NAV BAR
+  onHandleClick(linkName: string) {
+    this.activeLinks = [];
+    this.activeLinks[this.linkNames.indexOf(linkName)] = 'active';
+    
+  }
 }
